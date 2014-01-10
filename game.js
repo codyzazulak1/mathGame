c05f2060f2c97f8858a5ae5833b7c01f07d4f2d7
codyzazulak1 <codyzazulak1@gmail.com>
2014-01-10 04:39:04 +0000

    DONE

--- game.js
+++ game.js
@@ -4,124 +4,118 @@ var PLAYER_2 = 2;
 var p1Lives = 3;
 var p2Lives = 3;
 
-var firstRandom = Math.floor(Math.random()*21)
-var secondRandom = Math.floor(Math.random()*21)
-var operation = Math.floor(Math.random()*3)
-var trueAddAnswer = firstRandom + secondRandom;
-var trueSubAnswer = firstRandom - secondRandom;
-var trueMultiAnswer = firstRandom * secondRandom;
-var trueAnswer = trueMultiAnswer || trueSubAnswer || trueAddAnswer;
-var playerAnswer = 0;
+var firstRandom 	= Math.floor(Math.random()*21);
+var secondRandom 	= Math.floor(Math.random()*21);
+var operation 		= Math.floor(Math.random()*3);
 
 var turn = PLAYER_1;
+var playerAnswer 	= 0;
 
+function trueAddAnswer () 	{ return (firstRandom + secondRandom) == playerAnswer };
+function trueSubAnswer () 	{ return (firstRandom - secondRandom) == playerAnswer };
+function trueMultiAnswer () { return (firstRandom * secondRandom) == playerAnswer};
+function trueAnswer () 		{ return trueMultiAnswer() || trueSubAnswer() || trueAddAnswer() };
+
+function setNumbers() {
+	firstRandom 	= Math.floor(Math.random()*21);
+	secondRandom 	= Math.floor(Math.random()*21);
+	operation 		= Math.floor(Math.random()*3);
+}
 
 function gameLoop () {
 	//-----------//
-	var playOrNot = prompt("Play? Y/N");
+	var playOrNot = prompt("Play? Y/N (if 'N', refresh to restart game)");
 	if(playOrNot == "Y") {
-		console.log("wants to play")
-	}
-	else if(playOrNot == "N") {
-		console.log("close tab")
-	}
-	else {
-		console.log("idiot")
-	}
+		while(p1Lives > 0 && p2Lives > 0) {
+			setNumbers();
+			chooseMath(); 
 
-	//MAIN LOOP
-	while(p1Lives > 0 && p2Lives > 0) {
-		chooseMath() 
-
-		if (turn == PLAYER_1) {
-			answerForP1();
-		} else {
-			answerForP2();
+			if (turn == PLAYER_1) {
+				answerForP1();
+			} else {
+				answerForP2();
+			}	
 		}
 
+		p1GameOver();
+		p2GameOver();
 		
-
-
+		gameLoop();
 	}
+	else {
+		closeWindow();
+	}
+}
 
-	p1GameOver();
-	p2GameOver();
-
-	gameLoop();
+function closeWindow () {
+	if (confirm("Sure?")) {
+			window.close();
+		}
 }
 
 
-
-
-
-
-
-
-
- //CHOOSING ADD, SUB, MULTI
 function chooseMath() {
 	console.log(operation);
 	if(operation == 1) {
-		console.log("wants addition");
-		console.log(trueAddAnswer)
+		console.log("1 = wants addition");
+		console.log("answer = " + trueAddAnswer());
 		playerAnswer = prompt(firstRandom + " + " + secondRandom + " = " );
 	}
 	else if(operation == 2) {
-		console.log("wants substraction");
-		console.log(trueSubAnswer)
+		console.log("2 = wants substraction");
+		console.log("answer = " + trueSubAnswer());
 		playerAnswer = prompt(firstRandom + " - " + secondRandom + " = " );
 	}
 	else {
-		console.log("wants multiplication");
-		console.log(trueMultiAnswer)
+		console.log("0 = wants multiplication");
+		console.log("answer = " + trueMultiAnswer());
 		playerAnswer = prompt(firstRandom + " * " + secondRandom + " = " );
 	}
 }
-//SUCCESSFUL
 
 
-//------------
-
-//PLAYER ANSWERS
 function answerForP1() {
-	if (player1Answer != trueAnswer) {
-		p1Lives -= 1; 
+	if ( !trueAnswer() ) {
+		p1Lives -= 1;
+		alert("Wrong! Health -1. Click 'OK' to continue. Player 2, get ready.");
 	}
 	else {
-		alert("Right! Click 'OK' to continue");
+		alert("Right! Click 'OK' to continue. Player 2, get ready.");
 	}
-	console.log(p1Lives)
+	turn = PLAYER_2;
+	console.log(p1Lives);
 }
 
 function answerForP2() {
-	if (player2Answer != trueAnswer) {
+	if ( !trueAnswer() ) {
 		p2Lives -= 1;
+		alert("Wrong! Health -1. Click 'OK' to continue. Player 1, get ready.");
 	}
 	else {
-		alert("Right! Click 'OK' to continue");
+		alert("Right! Click 'OK' to continue. Player 1, get ready.");
 	}
-	console.log(p2Lives)
+	turn = PLAYER_1;
+	console.log(p2Lives);
 }
 
-//SUCCESSFUL
 
-//-----------
-
-//GAME OVER
 function p1GameOver() {
 if(p1Lives == 0) {
 	alert("Player 2 WINS!");
-	alert("Refresh to play again!")
+	alert("Click 'OK' to play again!");
 }
 }
 
 function p2GameOver() {
 if(p2Lives == 0) {
 	alert("Player 1 WINS!");
-	alert("Refresh to play again!")
+	alert("Click 'OK' to play again!");
 }
 }
-//NOT FULLY WORKING
+
+window.onload = function () {
+	gameLoop();
+};
 
 
 
